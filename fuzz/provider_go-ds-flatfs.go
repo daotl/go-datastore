f2e015e4665a3ec2144ff2bb1a50c89254e04b8a
Nex Zhu <4370605+NexZhu@users.noreply.github.com>
2021-03-27 15:24:17 +0800

    fix: update go-ds-flatfs usage in fuzz

--- fuzz/provider_go-ds-flatfs.go
+++ fuzz/provider_go-ds-flatfs.go
@@ -7,12 +7,13 @@ package fuzzer
 
 import (
 	ds "github.com/daotl/go-datastore"
+	"github.com/daotl/go-datastore/key"
 	prov "github.com/daotl/go-ds-flatfs"
 )
 
 func init() {
 	AddOpener("go-ds-flatfs", func(loc string) ds.Datastore {
-		d, err := prov.CreateOrOpen(loc, prov.IPFS_DEF_SHARD, false)
+		d, err := prov.CreateOrOpen(key.KeyTypeString, loc, prov.IPFS_DEF_SHARD, false)
 		if err != nil {
 			panic("could not create db instance")
 		}
