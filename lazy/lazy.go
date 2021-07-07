1e3f919f9db074fe5f56bcbbae1e7dabd144610f
Hu Yingcong <me@huyc.info>
2021-07-08 01:37:26 +0800

    1. add EnsureActive; 2. unexposed lockActive and unlockActive; 3. rename DSModifier to Action; 4. update some comments; 5. return ErrClosed in a closed LazyDatastore.

--- lazy/lazy.go
+++ lazy/lazy.go
@@ -14,21 +14,21 @@ var (
 	ErrClosed = errors.New("datastore closed")
 )
 
-type DSModifier func(d *datastore.Datastore) error
-
 // LazyDatastore wraps a datastore with three states: active, inactive and closed.
-// LazyDatasotre is inited by four DSModifier: init, activate, deactive and closeFunc.
-// the relationship between states and DSModifiers is as follows:
+// LazyDatasotre is inited by four Actions init, activate, deactive and closeFunc.
+// the relationship between states and Actions is as follows:
 //
 //                       -----------(deactivateFunc)----------
-//                       |                                   |
-//                       V                                   |
-// nil -(initFunc)-> deactive -----(activateFunc)---------> active
+//                       |                                   |  -(ensureActive)-
+//                       V                                   |  |              |
+// nil -(initFunc)-> inactive -----(activateFunc)---------> active <------------
 //                       |                                    |
 //                       --(closeFunc)-> closed <-(closeFunc)--
 //
-// Every call of lazyDatastore will make sure datastore is active, otherwise an
-// error will occur.
+// Caller can modify the child datastore in action, but caller is also
+// responsible to make sure the child datastore is callable.
+// Every call of lazyDatastore's datastore Interface will make sure datastore is
+// in active state, otherwise an error will occur.
 
 type LazyDatastore struct {
 	child          datastore.Datastore
@@ -36,16 +36,19 @@ type LazyDatastore struct {
 	writing        bool
 	active         bool
 	closed         bool
-	activateFunc   DSModifier
-	deactivateFunc DSModifier
-	closeFunc      DSModifier
+	activateFunc   Action
+	deactivateFunc Action
+	closeFunc      Action
 }
+
+// Action is the lazyDatastore's state transfer function
+type Action func(d *datastore.Datastore) error
 
 func NewLazyDataStore(
 	initFunc,
 	activateFunc,
 	deactivateFunc,
-	closeFunc DSModifier) (*LazyDatastore, error) {
+	closeFunc Action) (*LazyDatastore, error) {
 	var d datastore.Datastore
 	if initFunc == nil || activateFunc == nil || deactivateFunc == nil || closeFunc == nil {
 		return nil, fmt.Errorf("nil modifier")
@@ -66,11 +69,67 @@ func NewLazyDataStore(
 	}, nil
 }
 
-// LockActive check if l is opened and lock the datastore to active state.
-// If l is opened, prevent datastore's state being changed;
-// If l is not opened, change l.rw to exclusive(write) lock and open it.
-// Return any error when opening the datastore.
-func (l *LazyDatastore) LockActive() error {
+// EnsureActive runs op and makes sure that the wrapped datastore.Datastore is in active state.
+// EnsureActive returns any error if activation fails or op returns non-nil error.
+func (l *LazyDatastore) EnsureActive(op Action) error {
+	err := l.lockOrTransToActive()
+	defer l.unlockActive()
+	if err != nil {
+		return err
+	}
+	return op(&l.child)
+}
+
+// Activate lazyDatastore.
+// Activate doesn't ensure lazyDatastore keeping in active state.
+// To keep in active state, use EnsureActive instead.
+func (l *LazyDatastore) Activate() error {
+	return l.EnsureActive(func(d *datastore.Datastore) error { return nil })
+}
+
+// Deactivate lazyDatastore.
+func (l *LazyDatastore) Deactivate() error {
+	l.rw.Lock()
+	defer l.rw.Unlock()
+	if l.closed {
+		return ErrClosed
+	}
+	if !l.active {
+		return nil // already deactivate
+	}
+	err := l.deactivateFunc(&l.child)
+	if err != nil {
+		return err
+	}
+	l.active = false
+	return nil
+}
+
+// Close the lazyDatastore.
+// Return ErrClosed after being closed.
+// Close will NOT called the child's closer.
+// Close it in closeFunc.
+func (l *LazyDatastore) Close() error {
+	l.rw.Lock()
+	defer l.rw.Unlock()
+	if l.closed {
+		return ErrClosed
+	}
+	err := l.closeFunc(&l.child)
+	if err != nil {
+		return err
+	}
+	l.closed = true
+	return nil
+}
+
+// lockOrTransToActive checks if l is active and locks the datastore to active state.
+// If l is active, prevent datastore's state being changed;
+// If l is not active, change l.rw to exclusive(write) lock and activate it.
+// Return any error while activating.
+// It is caller's responsibility to make sure to call unlockActive in a same goroutine
+// after calling lockOrTransToActive.
+func (l *LazyDatastore) lockOrTransToActive() error {
 	l.rw.RLock()
 	if l.active {
 		return nil
@@ -95,8 +154,8 @@ func (l *LazyDatastore) LockActive() error {
 	return nil
 }
 
-// UnlockActive releases datastore.
-func (l *LazyDatastore) UnlockActive() {
+// unlockActive releases datastore.
+func (l *LazyDatastore) unlockActive() {
 	if !l.writing {
 		l.rw.RUnlock()
 	} else {
@@ -105,102 +164,59 @@ func (l *LazyDatastore) UnlockActive() {
 	}
 }
 
-// Deactivate lazyDatastore.
-func (l *LazyDatastore) Deactivate() error {
-	l.rw.Lock()
-	defer l.rw.Unlock()
-	if l.closed {
-		return ErrClosed
-	}
-	if !l.active {
-		return nil // already deactivate
-	}
-	err := l.deactivateFunc(&l.child)
-	if err != nil {
-		return err
-	}
-	l.active = false
-	return nil
-}
-
-// Close the lazyDatastore.
-func (l *LazyDatastore) Close() error {
-	l.rw.Lock()
-	defer l.rw.Unlock()
-	err := l.closeFunc(&l.child)
-	if err != nil {
-		return err
-	}
-	l.closed = true
-	return nil
-}
-
 // Put implements Datastore.Put
 func (l *LazyDatastore) Put(key key.Key, value []byte) (err error) {
-	err = l.LockActive()
-	defer l.UnlockActive()
-	if err != nil {
-		return err
-	}
-	return l.child.Put(key, value)
+	return l.EnsureActive(func(d *datastore.Datastore) error {
+		return (*d).Put(key, value)
+	})
 }
 
 // Sync implements Datastore.Sync
 func (l *LazyDatastore) Sync(prefix key.Key) error {
-	err := l.LockActive()
-	defer l.UnlockActive()
-	if err != nil {
-		return err
-	}
-	return l.child.Sync(prefix)
+	return l.EnsureActive(func(d *datastore.Datastore) error {
+		return (*d).Sync(prefix)
+	})
 }
 
 // Get implements Datastore.Get
 func (l *LazyDatastore) Get(key key.Key) (value []byte, err error) {
-	err = l.LockActive()
-	defer l.UnlockActive()
-	if err != nil {
-		return []byte{}, err
-	}
-	return l.child.Get(key)
+	err = l.EnsureActive(func(d *datastore.Datastore) error {
+		value, err = (*d).Get(key)
+		return err
+	})
+	return value, err
 }
 
 // Has implements Datastore.Has
 func (l *LazyDatastore) Has(key key.Key) (exists bool, err error) {
-	err = l.LockActive()
-	defer l.UnlockActive()
-	if err != nil {
-		return false, err
-	}
-	return l.child.Has(key)
+	err = l.EnsureActive(func(d *datastore.Datastore) error {
+		exists, err = (*d).Has(key)
+		return err
+	})
+	return exists, err
 }
 
 // GetSize implements Datastore.GetSize
 func (l *LazyDatastore) GetSize(key key.Key) (size int, err error) {
-	err = l.LockActive()
-	defer l.UnlockActive()
-	if err != nil {
-		return 0, err
-	}
-	return l.child.GetSize(key)
+	err = l.EnsureActive(func(d *datastore.Datastore) error {
+		size, err = (*d).GetSize(key)
+		return err
+	})
+	return size, err
 }
 
 // Delete implements Datastore.Delete
 func (l *LazyDatastore) Delete(key key.Key) (err error) {
-	err = l.LockActive()
-	defer l.UnlockActive()
-	if err != nil {
-		return err
-	}
-	return l.child.Delete(key)
+	return l.EnsureActive(func(d *datastore.Datastore) error {
+		return (*d).Delete(key)
+	})
 }
 
 // Query implements Datastore.Query
 func (l *LazyDatastore) Query(q query.Query) (rs query.Results, err error) {
-	err = l.LockActive()
-	defer l.UnlockActive()
-	if err != nil {
-		return nil, err
-	}
-	return l.child.Query(q)
+	err = l.EnsureActive(func(d *datastore.Datastore) error {
+		rs, err = (*d).Query(q)
+		return err
+	})
+	return rs, err
 }
